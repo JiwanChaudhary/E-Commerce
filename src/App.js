9325ecc9d3c6273d9a9721eb4625d0843e966b14
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2022-07-23 00:18:45 +0545

    project setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import { Button, Container } from "@mui/material";
+import { ThemeProvider } from "@mui/system";
 import { useEffect } from "react";
-
+import theme from "./styles/theme";
 function App() {
   useEffect(() => {
-    document.title = ("Mui Ecommerce");
+    document.title = "Mui Ecommerce";
   }, []);
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
       <Container maxWidth="xl" sx={{ background: "#fff" }}>
         <Button variant="contained">Test</Button>
       </Container>
-    </div>
+    </ThemeProvider>
   );
 }
 
