05610be63cbe25e7fab9d2d56c2fed8701226575
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2022-07-22 23:54:42 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Container } from "@mui/material";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    document.title = ("Mui Ecommerce");
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container maxWidth="xl" sx={{ background: "#fff" }}>
+        <Button variant="contained">Test</Button>
+      </Container>
     </div>
   );
 }
