2484faf476ff95613ed941c5a26d72850f839e98
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2022-07-26 23:26:40 +0545

    products data added

--- src/components/products/index.js
+++ src/components/products/index.js
@@ -1,7 +1,24 @@
-
+import { Grid, useMediaQuery } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import { products } from "../../data";
 
 export default function Products() {
-    return(
-        <h1>Products</h1>
-    );
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("md"));
+
+  const renderProducts = products.map((product) => (
+    <Grid
+      key={product.id}
+      display="flex"
+      flexDirection={"column"}
+      alignItems="center"
+    ></Grid>
+  ));
+
+  return (
+    <h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+      exercitationem veniam qui nihil libero.
+    </h1>
+  );
 }
---
+++ src/data/index.js
@@ -0,0 +1,50 @@
+export const products = [
+  {
+    id: 1,
+    name: "Super Bagpack",
+    price: 129.99,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi exercitationem veniam qui nihil libero.",
+    image: "/images/products/bag_01.png",
+  },
+  {
+    id: 2,
+    name: "New Hip",
+    price: 199.99,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi exercitationem veniam qui nihil libero.",
+    image: "/images/products/bag_02.png",
+  },
+  {
+    id: 3,
+    name: "Elite Series",
+    price: 189.99,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi exercitationem veniam qui nihil libero.",
+    image: "/images/products/bag_03.png",
+  },
+  {
+    id: 4,
+    name: "Casual",
+    price: 129.99,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi exercitationem veniam qui nihil libero.",
+    image: "/images/products/bag_04.png",
+  },
+  {
+    id: 5,
+    name: "Best Tote",
+    price: 399.99,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi exercitationem veniam qui nihil libero.",
+    image: "/images/products/bag_05.png",
+  },
+  {
+    id: 6,
+    name: "Charming Series",
+    price: 899.99,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi exercitationem veniam qui nihil libero.",
+    image: "/images/products/bag_06.png",
+  },
+];
