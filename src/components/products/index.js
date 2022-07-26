d17718c66a06723ae8923acf5f91d9f2c1ba2cb1
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2022-07-27 00:37:12 +0545

    products data initially rendered

--- src/components/products/index.js
+++ src/components/products/index.js
@@ -1,6 +1,7 @@
-import { Grid, useMediaQuery } from "@mui/material";
+import { Container, Grid, useMediaQuery } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import { products } from "../../data";
+import SingleProduct from "./SingleProduct";
 
 export default function Products() {
   const theme = useTheme();
@@ -8,17 +9,25 @@ export default function Products() {
 
   const renderProducts = products.map((product) => (
     <Grid
+      item
       key={product.id}
       display="flex"
       flexDirection={"column"}
       alignItems="center"
-    ></Grid>
+    >
+      <SingleProduct product={product} matches={matches} />
+    </Grid>
   ));
 
   return (
-    <h1>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
-      exercitationem veniam qui nihil libero.
-    </h1>
+    <Container>
+      <Grid
+        container
+        justifyContent={"center"}
+        sx={{ margin: "20px 4px 10px 4px" }}
+      >
+        {renderProducts}
+      </Grid>
+    </Container>
   );
 }
